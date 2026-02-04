c4cc613a5d063facb993da2081bc4aabdbe1efee
Ayyoub EL Kouri <Ayyoubelkouri@gmail.com>
2026-02-04 18:21:18 +0100

    feat(shell): Implement a global routing system with layouts to support multiple apps

---
+++ src/shell/navigation.store.ts
@@ -0,0 +1,13 @@
+import { create } from "zustand";
+
+export type RouteId = "idea" | "notes" | "settings" | "test";
+
+interface NavStore {
+  route: RouteId;
+  setRoute: (r: RouteId) => void;
+}
+
+export const useNavStore = create<NavStore>(set => ({
+  route: "idea",
+  setRoute: (route) => set({ route }),
+}));
